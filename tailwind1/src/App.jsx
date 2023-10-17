6a218b33718a8e8a5252a880158096e800c6c03a
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-10-17 11:07:24 +0530

    updated

--- tailwind1/src/App.jsx
+++ tailwind1/src/App.jsx
@@ -8,7 +8,21 @@ function App() {
 
   return (
     <>
-      <div className="text-white bg-slate-900">hello world</div>
+      <div className='flex h-screen  m-0 items-center justify-center'>
+
+        <div className='border-2 h-3/5 rounded-2xl w-80 flex flex-col bg-slate-500'>
+
+
+          <div className=' rounded-tr-2xl rounded-tl-2xl h-1/4 bg-slate-900'></div>
+
+
+        </div>
+
+      </div>
+
+
+
+
     </>
   )
 }
